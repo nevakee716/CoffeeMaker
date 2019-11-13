16ae941ec8e8bd73b046e3f9c22f8c00cd898b20
nevakee716 <nevakee716@gmail.com>
2019-11-13 15:17:54 +0100

    support page with tabs

--- src/RedirectEdit/RedirectManager.js
+++ src/RedirectEdit/RedirectManager.js
@@ -52,6 +52,7 @@
       }
     } else {
       sessionStorage.removeItem("lastUrl");
+      sessionStorage.removeItem("lastItem");
     }
     targetRedirection.view = view;
     return targetRedirection;
@@ -67,6 +68,73 @@
       }
     }
     return null;
+  }
+
+  function getPageNavigationForSinglePage(item, objectTypeScriptName) {
+    var views, o, i, v, tabCount, viewsLoaded, currentViewName, navView, viewSchema, j, tab, navTab, questionnaireQueTab, questionnaireResultTab;
+    if (cwApi.cwConfigs.SingleViewsByObjecttype === null) {
+      return [];
+    }
+
+    views = cwApi.cwConfigs.SingleViewsByObjecttype[objectTypeScriptName.toLowerCase()];
+
+    viewsLoaded = views.map(function(v) {
+      return cwApi.getView(v);
+    });
+    viewsLoaded = cwApi.sortBy(viewsLoaded, function(v) {
+      if (cwApi.isUndefined(v)) {
+        cwApi.Log.Error("One or several views are not correct part of your views, please check case of the views to don't have duplicates, Please check part of these views : " + views.join(","));
+        return 0;
+      }
+      return v.Order;
+    });
+    currentViewName = cwApi.getQueryStringObject().cwview;
+    o = [];
+    for (i = 0; i < viewsLoaded.length; i += 1) {
+      tabCount = 0;
+      v = viewsLoaded[i];
+      if (!cwApi.isUndefined(v) && cwApi.cwUser.canAccessView(cwApi.currentUser, v) === true) {
+        navView = {};
+        navView.viewName = v.cwView;
+        navView.selected = v.cwView === currentViewName;
+        navView.tabs = [];
+        var displayPropertyObject = new cwAPI.CwDisplayProperties(v.name, false);
+        if (displayPropertyObject.formattedFieldValue.indexOf("|>BuiltInSite") === 0) {
+          navView.label = cwApi.mapToTranslation(v.name);
+        } else {
+          navView.label = displayPropertyObject.getDisplayString(item);
+        }
+        viewSchema = cwApi.ViewSchemaManager.getPageSchema(v.cwView);
+        if (viewSchema.Tab !== null) {
+          for (j = 0; j < viewSchema.Tab.Tabs.length; j += 1) {
+            tabCount += 1;
+            tab = viewSchema.Tab.Tabs[j];
+            navTab = {
+              // tab: tab,
+              id: tab.Id,
+              label: cwApi.mapToTranslation(tab.Name),
+            };
+            navView.tabs.push(navTab);
+          }
+        }
+        // Add object questionnaire tabs
+        if (cwApi.isQuestionnaireBehaviuor(viewSchema.Behaviours) && objectTypeScriptName.toLowerCase() !== cwApi.mmDefinition.OBJECTTYPE_SCRIPTNAME_QUESTIONNAIRE) {
+          questionnaireQueTab = {
+            id: "tab" + tabCount,
+            label: cwApi.mapToTranslation("Questionnaire"),
+          };
+          questionnaireResultTab = {
+            id: "tab" + (tabCount + 1),
+            label: cwApi.mapToTranslation("Result"),
+          };
+          navView.tabs.push(questionnaireQueTab);
+          navView.tabs.push(questionnaireResultTab);
+        }
+
+        o.push(navView);
+      }
+    }
+    return o;
   }
 
   cwApi.cwEditProperties.cwEditPropertyManagerDOM.prototype.setEditModeButtonsActions = function() {
@@ -119,10 +187,17 @@
             $("div.cw-edit-buttons").html(o.join(""));
             that.setEditModeButtonsActions();
             sessionStorage.setItem("lastUrl", cwApi.getURLHash().replace("#", ""));
+            sessionStorage.setItem("lastItem", JSON.stringify(that.editPropertyManager.item));
             cwApi.updateURLHash(url);
-          } else {
-            cwApi.cwNotificationManager.addError(message);
-            $("#top-actions").removeClass("edit-mode");
+
+            var $ulView = $('<ul class="navViews"></ul>');
+            var pageNavigation = getPageNavigationForSinglePage(that.editPropertyManager.item, that.editPropertyManager.item.objectTypeScriptName);
+            $("#page-options").html($ulView);
+            for (i = 0; i < pageNavigation.length; i += 1) {
+              if (pageNavigation[i].viewName === targetRedirection.view) {
+                outputPageNavigation(pageNavigation[i], $ulView, that.editPropertyManager.item);
+              }
+            }
           }
         }
 
@@ -130,6 +205,29 @@
       });
     }
   };
+
+  function outputPageNavigation(navView, $ulViewLocal, item) {
+    var $li = cwApi.cwDisplayManager.getViewNav$(navView.viewName, navView.label, "#");
+    cwApi.createMainAndSubNavMenus($li, $ulViewLocal, navView, item);
+
+    if (!$li.children("a").hasClass("hassub")) {
+      $li.children("a").on("click", function() {
+        cwApi.cwDisplayManager.setContentPage();
+      });
+      cwApi.cwDisplayManager.setContentPage();
+    }
+    if (navView.selected) {
+      selectNavView($li);
+    }
+  }
+
+  function outputPageNavigationForObjectPage(item, objectTypeScriptName, restrictToCurrentView) {
+    var pageNavigation, $ulView, i, $li;
+    cwApi.CwPendingEventsManager.setEvent("OutputPageNavigationForObjectPage");
+
+    cwApi.applyPageMenuJavascript();
+    cwApi.CwPendingEventsManager.deleteEvent("OutputPageNavigationForObjectPage");
+  }
 
   function getRedirectPage(parentId, parentObjectTypeId) {
     if (!cwApi.isUndefined(parentId) && !cwApi.isUndefined(parentObjectTypeId)) {
@@ -146,10 +244,18 @@
 
   cwApi.cwEditProperties.unsetEditMode = function() {
     cwApi.CwPendingEventsManager.setEvent("UnsetEditMode");
-    var regex, hash;
+    var regex, hash, item;
     if (cwApi.queryObject.isEditMode() && sessionStorage.hasOwnProperty("lastUrl")) {
       hash = sessionStorage.getItem("lastUrl");
+      item = JSON.parse(sessionStorage.getItem("lastItem"));
       sessionStorage.removeItem("lastUrl");
+      sessionStorage.removeItem("lastItem");
+      var $ulView = $('<ul class="navViews"></ul>');
+      var pageNavigation = getPageNavigationForSinglePage(item, item.objectTypeScriptName);
+      $("#page-options").html($ulView);
+      for (i = 0; i < pageNavigation.length; i += 1) {
+        outputPageNavigation(pageNavigation[i], $ulView, item);
+      }
     } else {
       hash = cwApi.getURLHash();
     }
@@ -158,6 +264,7 @@
     hash = hash.replace(/\&cwisnew=true/g, "").replace("#", "");
 
     cwApi.updateURLHash(hash);
+
     cwApi.CwPendingEventsManager.deleteEvent("UnsetEditMode");
   };
 
