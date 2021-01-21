b58e79349c29af31f3332d156dd3069f7f380c90
nevakee716 <nevakee716@gmail.com>
2021-01-21 22:10:18 +0100

    fix date issue

--- src/workflow/cwWorkflow.angularDirective.js
+++ src/workflow/cwWorkflow.angularDirective.js
@@ -315,5 +315,22 @@
         },
       ]);
     });
+
+    cwApi.ngDirectives.push(function () {
+      loader.registerDirective("formatDate", function () {
+        return {
+          require: "ngModel",
+          link: function (scope, elem, attr, modelCtrl) {
+            modelCtrl.$formatters.push(function (modelValue) {
+              if (modelValue) {
+                return new Date(modelValue);
+              } else {
+                return null;
+              }
+            });
+          },
+        };
+      });
+    });
   }
 })(cwAPI, jQuery);
