1305aa08199fe593715f32807494368f21a8d38a
nevakee716 <nevakee716@gmail.com>
2021-01-22 12:20:37 +0100

    fix missing var

--- src/workflow/cwWorkflow.js
+++ src/workflow/cwWorkflow.js
@@ -511,7 +511,7 @@
     if (cwAPI.isDebugMode() === true) {
       self.load();
     } else {
-      libToLoad = ["modules/vis/vis.min.js", "modules/bootstrap/bootstrap.min.js", "modules/bootstrap-select/bootstrap-select.min.js"];
+      var libToLoad = ["modules/vis/vis.min.js", "modules/bootstrap/bootstrap.min.js", "modules/bootstrap-select/bootstrap-select.min.js"];
       // AsyncLoad
       cwApi.customLibs.aSyncLayoutLoader.loadUrls(libToLoad, function (error) {
         if (error === null) {
