2eaaf98da01ff88d1b72bb322514423529d9154c
nevakee716 <nevakee716@gmail.com>
2021-03-05 00:25:03 +0100

    Add memotext glossary

--- src/property/property.js
+++ src/property/property.js
@@ -96,7 +96,7 @@
           value = cwPropertiesGroups.types.lookupValue(value, property.valueID, config, noValue);
           break;
         case "Memo":
-          value = cwPropertiesGroups.formatMemoProperty(value);
+          value = cwPropertiesGroups.formatMemoProperty(value, objectTypeScriptName);
           break;
         case "Frequency":
         case "Duration":
@@ -199,6 +199,221 @@
       result += "</span>";
     }
     return result;
+  };
+
+  cwApi.cwPropertiesGroups.formatMemoProperty = function (value, objectTypeScriptName) {
+    if (value.indexOf("!DOCTYPE HTML PUBLIC") === -1 && value.indexOf("!DOCTYPE html") === -1) {
+      if (value.indexOf('"') !== -1) {
+        value = value.replace(/\\"/g, '"');
+      }
+
+      if (value.indexOf("\\") !== -1) {
+        value = value.replace(/\\\\/g, "\\");
+      }
+
+      value = cwApi.cwEditProperties.safe_tags(value);
+      //restore the <br/>
+      value = value.replace(/\&lt;br\/\&gt;/g, "<br/>");
+      // value = value.replace(/ /g, '&nbsp;');
+    }
+    let config = null;
+    if (cwAPI.customLibs.utils && cwAPI.customLibs.utils.getCustomLayoutConfiguration) {
+      config = cwAPI.customLibs.utils.getCustomLayoutConfiguration("property");
+    }
+    if (config && objectTypeScriptName && config.objectTypeUsedForMemoDefinitionMapping[objectTypeScriptName]) {
+      let view = config.objectTypeUsedForMemoDefinitionMapping[objectTypeScriptName];
+      if (!cwApi.queryObject.isEditMode()) {
+        try {
+          if (!cwApi.customLibs.utils.glossary[view]) {
+            let jsonFile = cwApi.getIndexViewDataUrl(view);
+            var request = new XMLHttpRequest();
+            request.open("GET", jsonFile, false); // `false` makes the request synchronous
+            request.send(null);
+            if (request.status === 200 && status != "Ko") {
+              let jsonRep = JSON.parse(request.responseText);
+              cwApi.customLibs.utils.glossary[view] = jsonRep[Object.keys(jsonRep)[0]];
+            }
+          }
+          var cdsByID = {};
+          cwApi.customLibs.utils.glossary[view]
+            .sort(function (a, b) {
+              return b.name.length - a.name.length;
+            })
+            .forEach(function (obj) {
+              var reg = new RegExp("([^A-Z])(" + obj.name + ")([^A-Z])", "ig");
+              cdsByID[obj.object_id] = {
+                cds: cwAPI.customLibs.utils.getItemDisplayString(view, obj),
+                name: obj.name,
+              };
+
+              value = value.replace(reg, "$1#id#" + obj.object_id + "#id#$3");
+            });
+
+          Object.keys(cdsByID).forEach(function (id) {
+            value = value.replaceAll("#id#" + id + "#id#", "<delEdit name='" + cdsByID[id].name + "'>" + cdsByID[id].cds + "</delEdit>");
+          });
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    }
+    return value;
+  };
+
+  cwApi.cwEditProperties.cwEditPropertyMemo.prototype.setEditModeInDOM = function () {
+    var o, value;
+    o = [];
+    value = this.initialDOMContent;
+    let regex = /<deledit.*?name="(.*?)".*?<\/deledit>/gi;
+
+    value = value.replace(regex, "$1");
+
+    if (this.p.newMemoControl === true) {
+      value = cwApi.cwEditProperties.cwEditPropertyMemo.replaceNewlinesWithBR(value);
+    }
+    if (cwApi.cwEditProperties.cwEditPropertyMemo.isHTMLContent(value) === false || this.p.newMemoControl === true) {
+      o.push("<textarea ", this.createPropertyAttributesInline(), ">", value, "</textarea>");
+      this.propertyDOM.html(o.join(""));
+
+      if (this.p.newMemoControl === true) {
+        tinyMCE.dom.Event.domLoaded = true;
+        cwApi.cwTinymceManager.initalizeTinymce("cw-edit-attr-" + this.id, function (ed) {
+          //this tinymce initialize + after loaded set the initial values
+          var propertyId = String(ed.id).replace("cw-edit-attr-", "");
+          for (var i = 0; i < cwApi.editPropertiesManager.properties.length; i++) {
+            if (String(cwApi.editPropertiesManager.properties[i].id) === propertyId) {
+              var scriptName = cwApi.editPropertiesManager.properties[i].scriptName;
+              if (!cwAPI.isUndefined(cwApi.editPropertiesManager.initialValues))
+                cwApi.editPropertiesManager.initialValues.properties[scriptName] = cwApi.cwTinymceManager.getFormattedHtml(ed.id);
+            }
+          }
+        });
+      }
+    }
+    this.setMandatoryPropertyDOM();
+  };
+
+  var updatePropertyForGridReview = function (
+    gridItem,
+    mainItem,
+    sourceItem,
+    targetItem,
+    displayNames,
+    item,
+    propertyScriptName,
+    propertyFromMetaModel,
+    propertiesLocation
+  ) {
+    if (propertyFromMetaModel.readOnly !== true) {
+      sourceItem.properties[propertyScriptName] = mainItem[propertiesLocation][propertyScriptName];
+
+      //Fix RFI Create then Edit
+      if (propertyScriptName === "functionsynonym" && cwApi.isUndefined(sourceItem.properties[propertyScriptName])) {
+        sourceItem.properties[propertyScriptName] = "";
+      }
+
+      if (propertyScriptName === "exportflag") {
+        sourceItem.lookupSelectedText[propertyScriptName] = cwApi.cwPropertiesGroups.assignFreezeLevel(
+          mainItem[propertiesLocation][propertyScriptName]
+        );
+        targetItem.lookupSelectedText[propertyScriptName] = cwApi.cwPropertiesGroups.assignFreezeLevel(gridItem[propertyScriptName]);
+      } else if (propertyScriptName === "whoowns") {
+        sourceItem.lookupSelectedText[propertyScriptName] = cwApi.cwConfigs.UserData.UserById[mainItem[propertiesLocation][propertyScriptName]];
+        targetItem.lookupSelectedText[propertyScriptName] = cwApi.cwConfigs.UserData.UserById[gridItem[propertyScriptName]];
+      }
+      displayNames[propertyScriptName] = propertyFromMetaModel.name;
+
+      if (propertyFromMetaModel.type === "Lookup") {
+        sourceItem.properties[propertyScriptName] = mainItem[propertiesLocation][propertyScriptName + "_id"];
+        propertyScriptName = propertyScriptName + "_id";
+      } else if (propertyFromMetaModel.type === "Memo") {
+        sourceItem.properties[propertyScriptName] = cwAPI.cwPropertiesGroups.formatMemoProperty(mainItem[propertiesLocation][propertyScriptName]);
+      } else if (propertyFromMetaModel.type === "Boolean") {
+        sourceItem.lookupSelectedText[propertyScriptName] = cwApi.cwPropertiesGroups.types.booleanValue(
+          mainItem[propertiesLocation][propertyScriptName]
+        );
+        targetItem.lookupSelectedText[propertyScriptName] = cwApi.cwPropertiesGroups.types.booleanValue(gridItem[propertyScriptName]);
+      } else if (propertyFromMetaModel.type === "Date") {
+        sourceItem.lookupSelectedText[propertyScriptName] = cwApi.cwPropertiesGroups.types.strictDateValue(
+          mainItem[propertiesLocation][propertyScriptName]
+        );
+        targetItem.lookupSelectedText[propertyScriptName] = cwApi.cwPropertiesGroups.types.strictDateValue(gridItem[propertyScriptName]);
+      }
+
+      if (!cwApi.isUndefined(gridItem[propertyScriptName])) {
+        targetItem.properties = gridItem.checkPropertyType(
+          propertyFromMetaModel,
+          sourceItem,
+          targetItem,
+          gridItem,
+          mainItem,
+          propertyScriptName,
+          propertyFromMetaModel
+        );
+      }
+    }
+  };
+
+  cwBehaviours.CwKendoGridItem.prototype.outputMainObjectProperties = function (gridItem, mainItem, sourceItem, targetItem, displayNames) {
+    var outputObject = new cwBehaviours.CwKendoGridData();
+
+    outputObject.outputPropertiesGroupsContent(mainItem, this.nodeSchema, "PropertiesGroups", function (item, propertyScriptName) {
+      var propertyFromMetaModel = cwApi.mm.getProperty(mainItem.objectTypeScriptName, propertyScriptName);
+      updatePropertyForGridReview(
+        gridItem,
+        mainItem,
+        sourceItem,
+        targetItem,
+        displayNames,
+        item,
+        propertyScriptName,
+        propertyFromMetaModel,
+        "properties"
+      );
+    });
+  };
+
+  cwBehaviours.CwKendoGridItem.prototype.outputintersectionObjectProperties = function (
+    gridItem,
+    mainItem,
+    sourceItem,
+    targetItem,
+    intersectionGridsByUid,
+    displayNames
+  ) {
+    var outputObject;
+    outputObject = new cwBehaviours.CwKendoGridData();
+    outputObject.outputPropertiesGroupsContent(mainItem, this.nodeSchema, "iPropertiesGroups", function (item, propertyScriptName) {
+      var propertyFromMetaModel = cwApi.mm.getProperty(mainItem.iObjectTypeScriptName, propertyScriptName);
+
+      updatePropertyForGridReview(
+        gridItem,
+        mainItem,
+        sourceItem,
+        targetItem,
+        displayNames,
+        item,
+        propertyScriptName,
+        propertyFromMetaModel,
+        "iProperties"
+      );
+
+      if (!cwApi.isUndefined(gridItem[propertyScriptName])) {
+        if (propertyScriptName === "name") {
+          targetItem.properties[propertyScriptName] = gridItem.Intersection_Name;
+        }
+      }
+
+      if (propertyScriptName === "functionsynonym") {
+        targetItem.properties[propertyScriptName] = gridItem.Intersection_Name;
+      }
+
+      displayNames[propertyScriptName] = propertyFromMetaModel.name;
+    });
+  };
+
+  cwBehaviours.CwKendoGridMemoType.prototype.setProperty = function (gridObject, item, propertyScriptName) {
+    gridObject[propertyScriptName] = cwAPI.cwPropertiesGroups.formatMemoProperty(item.properties[propertyScriptName], item.objectTypeScriptName);
   };
 
   cwApi.cwPropertiesGroups = cwPropertiesGroups;
