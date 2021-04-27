34be661baa93b11fd97c01831e3cefe806a4c7a9
nevakee716 <nevakee716@gmail.com>
2021-04-27 21:34:53 +0200

    Adding feature for unit display

--- src/property/property.js
+++ src/property/property.js
@@ -177,12 +177,40 @@
       if (
         (step.min && step.max === null && step.min < value) ||
         (step.min && step.max && step.min < value && step.max > value) ||
-        (step.max && step.min === null && step.max > value)
+        (step.max && step.min === null && step.max > value) ||
+        ([undefined, null, ""].indexOf(step.min) !== -1 && [undefined, null, ""].indexOf(step.max) !== -1)
       ) {
         selectedStep = step;
       }
     });
 
+    if (selectedStep) {
+      function number_format(number, decimals, dec_point, thousands_sep) {
+        var n = !isFinite(+number) ? 0 : +number,
+          prec = !isFinite(+decimals) ? 0 : Math.abs(decimals),
+          sep = typeof thousands_sep === "undefined" ? "," : thousands_sep,
+          dec = typeof dec_point === "undefined" ? "." : dec_point,
+          toFixedFix = function (n, prec) {
+            // Fix for IE parseFloat(0.55).toFixed(0) = 0;
+            var k = Math.pow(10, prec);
+            return Math.round(n * k) / k;
+          },
+          s = (prec ? toFixedFix(n, prec) : Math.round(n)).toString().split(".");
+        if (s[0].length > 3) {
+          s[0] = s[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, sep);
+        }
+        if ((s[1] || "").length < prec) {
+          s[1] = s[1] || "";
+          s[1] += new Array(prec - s[1].length + 1).join("0");
+        }
+        return s.join(dec);
+      }
+      let unit = selectedStep.unit ? selectedStep.unit : "";
+
+      value = number_format(value, undefined, selectedStep.decimalseparator, selectedStep.thousandseparator);
+
+      value = selectedStep.infront && unit ? unit + " " + value : value + " " + unit;
+    }
     return this.getResultForStyling(value, selectedStep, noValue);
   };
 
