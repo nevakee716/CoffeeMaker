430efd118f969bb7fb29599530807def11a7f29a
Nevakee716 <nevakee716@gmail.com>
2022-06-08 17:41:26 +0200

    fix issue with package of jspdf

--- jspdf/index.js
+++ jspdf/index.js
@@ -1,10 +1,8 @@
 /* eslint-disable no-console */
 import jsPDF from "jspdf";
 import autoTable from "jspdf-autotable";
-import html2canvas from "html2canvas";
 
 module.exports = {
-  jsPDF: jsPDF,
   autoTable: autoTable,
-  html2canvas: html2canvas,
+  jsPDF: jsPDF,
 };
--- jspdf/webpackConfig.js
+++ jspdf/webpackConfig.js
@@ -1,11 +1,12 @@
 const path = require("path");
 const webpack = require("webpack");
+const MergeIntoSingleFilePlugin = require("webpack-merge-and-include-globally");
 
 module.exports = {
   entry: { app: "./index.js" },
 
   output: {
-    filename: "jspdf.min.js",
+    filename: "jspdf.js",
     path: process.cwd() + "/../modules/jspdf/",
     publicPath: "",
     library: "cwJsPdf",
