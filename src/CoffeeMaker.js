619a5d3ae3410c305052ae5c86c52d3e43531dbc
Nevakee716 <nevakee716@gmail.com>
2022-10-21 11:33:59 +0200

    fix for evolve 2022

--- src/CoffeeMaker.js
+++ src/CoffeeMaker.js
@@ -97,6 +97,8 @@
       if (this.config.cwBackup === undefined) this.config.cwBackup = {};
       if (this.config.misc === undefined) this.config.misc = {};
       cwApi.customLibs.utils.customLayoutConfiguration = this.config;
+      cwApi.customLibs.utils.configurationVersionNumber = cwAPI.customLibs.utils.getWorkflowConfigVersionNumber ? cwAPI.customLibs.utils.getWorkflowConfigVersionNumber() : 42;
+
     }
   };
 
@@ -121,13 +123,13 @@
 
       output.push(
         '<div data-id="saveconfiguration" id="CoffeeMakerTab_saveconfiguration_"' +
-          self.nodeID +
-          '" class="CoffeeMakerTab"><i class="fa fa-floppy-o" aria-hidden="true"></i></div>'
+        self.nodeID +
+        '" class="CoffeeMakerTab"><i class="fa fa-floppy-o" aria-hidden="true"></i></div>'
       );
       output.push(
         '<div data-id="refresh" id="CoffeeMakerTab_localstorage_' +
-          self.nodeID +
-          '" class="CoffeeMakerTab"><i class="fa fa-refresh" aria-hidden="true"></i></div>'
+        self.nodeID +
+        '" class="CoffeeMakerTab"><i class="fa fa-refresh" aria-hidden="true"></i></div>'
       );
 
       output.push("</div>");
@@ -195,7 +197,28 @@
     );
   };
 
-  cwCoffeeMaker.prototype.loadAngularTemplate = function () {
+  cwCoffeeMaker.prototype.loadAngularTemplate = function (configName) {
+    if (cwAPI.cwConfigs.EnabledVersion.indexOf("v2022") !== -1) {
+      return this.loadAngularTemplate_new(configName);
+    } else {
+      return this.loadAngularTemplate_old(configName);
+    }
+  };
+
+  cwCoffeeMaker.prototype.loadAngularTemplate_new = function () {
+    var self = this;
+    var $container = $("#CoffeeMakerViewContainer");
+    cwApi.CwAsyncLoader.load("angular", function () {
+      var loader = cwApi.CwAngularLoader;
+      // get tab name while loading CwAdvWorkflowConfigurator layout view
+      var tabName = $('li[data-menu-id="CwAdvWorkflowConfigurator"]').find('span').text();
+      registerTabsEvent(loader, self, $container);
+      loadTemplate(loader, self, $container, tabName);
+      addControlButtons(self);
+    });
+  };
+
+  cwCoffeeMaker.prototype.loadAngularTemplate_old = function () {
     var self = this;
     var $container = $("#CoffeeMakerViewContainer_" + this.nodeID);
     cwApi.CwAsyncLoader.load("angular", function () {
@@ -346,6 +369,215 @@
       }
     });
   };
+
+
+  function loadTemplate(loader, self, $container, tabName) {
+    loader.setup();
+    var datasetId = "";
+    Object.keys(self.config).forEach(function (e) {
+      if (self.config[e] && tabName === $.i18n.prop(e)) {
+        datasetId = e;
+      }
+    });
+
+    let templatePath = "";
+    if (["redirectEdit", "duplicateButton", "homePage", "cdsEnhanced", "checkEditModel", "pageFilter", "tableComplexeEnhanced", "diagram", "property", "cwBackup", "misc"].indexOf(datasetId) === -1) {
+      templatePath = loader.prefixWithTemplatePath('coffee', datasetId) + "?" + Math.random();
+    } else {
+      templatePath = cwAPI.getCommonContentPath() + "/html/coffee/" + datasetId + ".ng.html" + "?" + Math.random();
+    }
+    if (datasetId) {
+      loader.loadControllerWithTemplate(datasetId, $container, templatePath, function ($scope) {
+        $scope.metamodel = cwAPI.mm.getMetaModel();
+        $scope.views = cwApi.cwConfigs.Pages;
+        self.angularScope = $scope;
+        $scope.ng = {};
+        $scope.ng.config = self.config[datasetId];
+        self.config[datasetId] = $scope.ng.config;
+        $scope.gConfig = self.config;
+        $scope.cwApi = cwApi;
+        $scope.lang = cwApi.getSelectedLanguage();
+        $scope.showDescription = true;
+        $scope.FilterOperators = ["=", "!=", ">", "<", "In"];
+        $scope.keys = function (o) {
+          return Object.keys(o);
+        };
+        $scope.$watchCollection("ng", function (newValue, oldValue) {
+          self.config[datasetId] = $scope.ng.config;
+        });
+        $scope.jsonText = {};
+        $scope.toggle = function (c, e) {
+          if (c.hasOwnProperty(e)) delete c[e];
+          else c[e] = true;
+        };
+
+        $scope.toggleArray = function (c, e) {
+          var i = c.indexOf(e);
+          if (i === -1) c.push(e);
+          else c.splice(i, 1);
+        };
+        $scope.configError = false;
+        $scope.bootstrapFilter = function (id, value) {
+          window.setTimeout(function (params) {
+            $("#" + id).selectpicker();
+            if (value) $("#" + id).selectpicker("val", value);
+          }, 1000);
+        };
+        $scope.objectToArray = function (objs) {
+          if (!objs) return [];
+          return Object.keys(objs).map(function (k) {
+            return objs[k];
+          });
+        };
+        $scope.getPropertyDataType = function (ot, scriptname) {
+          if (cwApi.isUndefined(ot)) {
+            return "";
+          }
+          if (scriptname) {
+            var p = cwApi.mm.getProperty(ot.scriptName, scriptname);
+            if (cwApi.isUndefined(p)) {
+              return "";
+            }
+            switch (p.type) {
+              case "Boolean":
+                return "checkbox";
+              case "Integer":
+              case "Double":
+                return "text";
+              case "Lookup":
+                return "lookup";
+              default:
+                return "text";
+            }
+          } else return "number";
+        };
+
+        $scope.getRootObjectTypeFromView = function (view) {
+          return cwApi.mm.getObjectType(
+            cwApi.getViewsSchemas()[view].NodesByID[cwApi.getViewsSchemas()[view].RootNodesId[0]].ObjectTypeScriptName
+          );
+        };
+
+        $scope.addSlide = function (display) {
+          if (display.slides === undefined) display.slides = [];
+          display.slides.push({ label: "Slide " + display.slides.length });
+          $scope.selectSlide(display, display.slides.length - 1);
+        };
+
+        $scope.removeSlide = function (display, i) {
+          display.slides.splice(i, 1);
+        };
+
+        $scope.selectSlide = function (display, i) {
+          display.slides.forEach(function (c, ii) {
+            if (i == ii) c.selected = true;
+            else c.selected = false;
+          });
+        };
+        $scope.getSortPropFromOT = function (ot) {
+          let r = [];
+          if (ot) {
+            for (let p in ot.properties) {
+              if (ot.properties.hasOwnProperty(p)) {
+                r.push(ot.properties[p]);
+              }
+            }
+            r.sort(function (pa, pb) {
+              return pa.name.localeCompare(pb.name);
+            });
+          }
+          return r;
+        };
+        $scope.reOrderSlides = function () {
+          $scope.ng.config.columns.sort(function (a, b) {
+            return a.order - b.order;
+          });
+        };
+
+        $scope.OTs = [];
+        $scope.objectTypes = cwAPI.mm.getMetaModel().objectTypes;
+        for (let o in $scope.objectTypes) {
+          if ($scope.objectTypes.hasOwnProperty(o) && !$scope.objectTypes[o].properties.hasOwnProperty("allowautomaticdeletion")) {
+            $scope.OTs.push($scope.objectTypes[o]);
+          }
+        }
+
+
+        if (self["controller_" + datasetId] && $scope.ng.config) self["controller_" + datasetId]($container, templatePath, $scope);
+      });
+    }
+  }
+
+  function registerTabsEvent(loader, self, $container) {
+    var matches = $('.cw-menu-zone .sub-level').find('a[href="#cwtype=ngsimple&cwview=CwAdvWorkflowConfigurator&cwmodule=ngDiagramEditor&lang=en"]').parent();
+    for (let i = 0; i < matches.length; i++) {
+      let tab = matches[i];
+      $(tab).off('click'); // to avoid multiple event listener
+      $(tab).click(function () {
+        let simpleQueryString = cwApi.cwPageManager.parseQueryString(window.location.hash);
+        if (simpleQueryString.cwview == "CwAdvWorkflowConfigurator") {
+          // get tab name when switching b/w tabs
+          let tabName = $(this).text();
+          loadTemplate(loader, self, $container, tabName);
+        }
+      });
+    }
+  }
+
+  function addControlButtons(self) {
+    let configVersionNoFromServer = cwAPI.customLibs.utils.getWorkflowConfigVersionNumber();
+    let configLocalKey = cwAPI.customLibs.utils.getConfigLocalStorageKey(configVersionNoFromServer);
+
+    $('.coffeeMaker-control-btn').remove();
+    //appending Save and Refresh workflow configuration buttons
+    var controlButtons = '<li data-id="saveconfiguration" title="Save Configuration" id="CoffeeMakerTab_saveconfiguration" class="page-top-li coffeeMaker-control-btn">'
+      + '<i class="btn page-action no-text fa fa-floppy-o" aria-hidden="true"></i>'
+      + '</li>'
+      + '<li data-id="refresh" title="Refresh Configuration" id="CoffeeMakerTab_localstorage" class="page-top-li coffeeMaker-control-btn">'
+      + '<i class="btn page-action no-text fa fa-refresh" aria-hidden="true"></i>'
+      + '</li>'
+      + '<li data-id="publishconfiguration" title="Publish Configuration" id="CoffeeMakerTab_publishconfiguration" class="page-top-li coffeeMaker-control-btn">'
+      + '<i class="btn page-action no-text fa fa-upload" aria-hidden="true" title="Publish"></i>'
+      + '</li>';
+
+    $('.page-top-ul').append(controlButtons);
+
+    $('.coffeeMaker-control-btn').click(function () {
+      if (this.dataset.id === "saveconfiguration" && self.objectConfigurationId) {
+        self.saveConfiguration(angular.toJson(self.config));
+      }
+      if (this.dataset.id === "saveconfiguration") {
+        //update config in local storage
+        if (configVersionNoFromServer > cwApi.customLibs.utils.configurationVersionNumber) {
+          //some new version is available on server
+          cwApi.notificationManager.addNotification($.i18n.prop("workflow_confiuration_outdated"), "error");
+          return;
+        }
+        localStorage.setItem(configLocalKey, angular.toJson(self.config));
+        cwApi.notificationManager.addNotification($.i18n.prop("workflow_confiuration_saved"));
+      }
+      if (this.dataset.id === "publishconfiguration") {
+        //add confirmation box before API AdvancedWorkflow/UpdateConfig
+        cwAPI.customLibs.utils.saveCustomLayoutConfiguration(self.config);
+        localStorage.removeItem(configVersionNoFromServer);
+        cwApi.customLibs.utils.customLayoutConfiguration = void 0; //setting undefined
+        //reload new values in local storage
+        cwAPI.customLibs.utils.getCustomLayoutConfiguration();
+        cwApi.notificationManager.addNotification($.i18n.prop("workflow_confiuration_published"));
+      }
+      if (this.dataset.id === "refresh") {
+        //remove localConfiguration, Fetch Configuration from API AdvancedWorkflow/GetConfig
+        let $container = $(".cw-adv-workflow");
+        $container.find(".cw-de-save-dialog").remove();
+        cwApi.outputConfirmationDialog($container, "Your draft changes will be lost. Do you want to continue ?", function () {
+          cwAPI.customLibs.utils.removeLocalConfiguration();
+          window.location.reload(true);
+        }, function () { });
+      }
+    });
+  }
+
+
 
   cwCoffeeMaker.prototype.unselectTabs = function (tabs) {
     let matches = document.querySelectorAll(".CoffeeMakerTab");
