16d714e87a99c4eba3277bd21a9038a658c1aa21
Nevakee716 <nevakee716@gmail.com>
2022-12-13 18:08:36 +0100

    fix nicelist directive

--- ngDirectives/niceList/niceList.ngDirective.js
+++ ngDirectives/niceList/niceList.ngDirective.js
@@ -2,17 +2,32 @@
 (function (cwApi, $) {
   "use strict";
   var loader = cwApi.CwAngularLoader;
-  var color = [{
-    "color": "#0094dc",
-    "background-color": "#e1f5ff"
-  },{
-    "color": "#13d613",
-    "background-color": "#ddffdd"
-  },{
-    "color": "#fd6c00",
-    "background-color": "#fef0e5"
-  }];
-
+  var color = [
+    {
+      color: "#0094dc",
+      "background-color": "#e1f5ff",
+    },
+    {
+      color: "#13d613",
+      "background-color": "#ddffdd",
+    },
+    {
+      color: "#fd6c00",
+      "background-color": "#fef0e5",
+    },
+    {
+      color: "#7300ff",
+      "background-color": "#F1E5FF",
+    },
+    {
+      color: "#16537E",
+      "background-color": "#E7EDF2",
+    },
+    {
+      color: "#FFC61A",
+      "background-color": "#FFF9E8",
+    },
+  ];
 
   if (cwApi.ngDirectives) {
     cwApi.ngDirectives.push(function () {
@@ -26,63 +41,69 @@
             } else {
               $scope.item.setAsFav = false;
             }
-            
+
             $scope.tagProperties = [];
 
-            Object.keys($scope.item.properties).filter(function(p) {
-              return ['name'].indexOf(p) === -1 && ['exportflag'].indexOf(p) === -1 && p.indexOf("_id") === -1 && p.indexOf("_abbreviation") === -1 && ['cwaveragerating'].indexOf(p) === -1 && ['cwtotalcomment'].indexOf(p) === -1;
-            }).forEach(function(ps){
-              let property = cwAPI.mm.getProperty($scope.item.objectTypeScriptName,ps);
-              if(property.type === "Memo") return ;
-              if(property.type === "URL") {
-                $scope.urlPropertiesScriptname = ps;
-                $scope.url = $scope.item.properties[$scope.urlPropertiesScriptname]
-              }
-              else if(property.type === "Date") {
-                $scope.datePropertiesScriptname = ps;
-                $scope.date = $scope.item.properties[$scope.datePropertiesScriptname]
-              } else {
-             
-                $scope.tagProperties.push($scope.item.properties[ps]);
-              }
-            })
+            Object.keys($scope.item.properties)
+              .filter(function (p) {
+                return (
+                  ["name"].indexOf(p) === -1 &&
+                  ["exportflag"].indexOf(p) === -1 &&
+                  p.indexOf("_id") === -1 &&
+                  p.indexOf("_abbreviation") === -1 &&
+                  ["cwaveragerating"].indexOf(p) === -1 &&
+                  ["cwtotalcomment"].indexOf(p) === -1
+                );
+              })
+              .forEach(function (ps) {
+                let property = cwAPI.mm.getProperty($scope.item.objectTypeScriptName, ps);
+                if (property.type === "Memo") return;
+                if (property.type === "URL") {
+                  $scope.urlPropertiesScriptname = ps;
+
+                  $scope.url = $scope.item.properties[$scope.urlPropertiesScriptname];
+                } else if (property.type === "Date") {
+                  $scope.datePropertiesScriptname = ps;
+                  $scope.datePropertiesName = cwApi.mm.getProperty($scope.item.objectTypeScriptName, ps).name;
+                  $scope.date = $scope.item.properties[$scope.datePropertiesScriptname];
+                } else {
+                  $scope.tagProperties.push($scope.item.properties[ps]);
+                }
+              });
 
             let config = cwAPI.customLibs.utils.getCustomLayoutConfiguration("cdsEnhanced");
             if (config) {
               if (config.displayPopoutByDefault) {
-          
                 let popOutName = cwApi.replaceSpecialCharacters($scope.item.objectTypeScriptName) + "_diagram_popout";
                 if (cwAPI.ViewSchemaManager.pageExists(popOutName) === true) {
-                  $scope.popout = {id:$scope.item.object_id,name:popOutName};
+                  $scope.popout = { id: $scope.item.object_id, name: popOutName };
                 }
               }
             }
 
-            $scope.openPopout = function(popout,id){
-              cwAPI.customFunction.openDiagramPopoutWithID(id,popout)
-            }
+            $scope.openPopout = function (popout, id) {
+              cwAPI.customFunction.openDiagramPopoutWithID(id, popout);
+            };
 
             $scope.associationsIcon = [];
             $scope.associations = [];
-            Object.keys($scope.item.associations).forEach(function(k){
-              
-              if(k.indexOf("_icon") !== -1){
+            Object.keys($scope.item.associations).forEach(function (k) {
+              if (k.indexOf("_icon") !== -1) {
                 $scope.associationsIcon.push($scope.item.associations[k]);
               } else {
                 $scope.associations.push($scope.item.associations[k]);
               }
-            })
+            });
 
-            $scope.getCDS = function(item){
+            $scope.getCDS = function (item) {
               return $scope.item.viewSchema.NodesByID[item.nodeID].LayoutOptions.DisplayPropertyScriptName;
-            }
+            };
 
             $scope.color = color;
             let options = { year: "numeric", month: "long", day: "numeric" };
-        
+
             let lDate = new Date($scope.date);
             $scope.lDate = lDate.toLocaleDateString(undefined, options);
-
 
             $scope.addAsFavourite = function () {
               $scope.item.setAsFav = true;
